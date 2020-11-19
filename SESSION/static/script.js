bcd8ae22f49b6bdacbef17b858b3a24554747262
anunay sinha <anusu90@gmail.com>
2020-11-19 10:23:14 +0530

    first commit

---
+++ SESSION/static/script.js
@@ -0,0 +1,10 @@
+console.log("hello")
+
+// https://github.com/rvsp/HTML-CSS/blob/master/Mini-tasks/002-HTML-CSS.md
+
+
+
+// https://cssgradient.io/https://www.css-gradient.com/
+// https://mycolor.space/gradient
+// https://html-css-js.com/css/generator/gradient/
+//https://docs.google.com/document/d/1f5nZnYev1R7k5MAP-kAS7lrRDgBHb2ca-trSj6OOg8k/edit
